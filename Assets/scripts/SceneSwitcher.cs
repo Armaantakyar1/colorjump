801bf0ee967ba54714a63f99510387ad7a5da789
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2023-02-22 13:14:07 +0400

    finish the game

--- Assets/scripts/SceneSwitcher.cs
+++ Assets/scripts/SceneSwitcher.cs
@@ -9,4 +9,8 @@ public class SceneSwitcher : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
